d063d837aa802ccd944ee2b7867bb988fd6945ce
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-08-23 20:04:03 +0400

    updated form setting

--- src/app/[locale]/5k-demo-trading-competition/deposit-match-bonus/components/HeroMargin.jsx
+++ src/app/[locale]/5k-demo-trading-competition/deposit-match-bonus/components/HeroMargin.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import DepositFormNew from "./DepositForm";
 
 const HeroMargin = ({ setIsOpen }) => {
-  const t = useTranslations("demo.banner");
+  const t = useTranslations("MarginContent.banner");
   const locale = useLocale();
   const [isRtl, setIsRtl] = useState(false);
 
@@ -18,54 +18,34 @@ const HeroMargin = ({ setIsOpen }) => {
   const imageSrc = isRtl ? "/demo/arabic.webp" : "/demo/full.webp";
 
   return (
-    <section className="bg-white py-8 md:py-20 xl:pt-20 xl:pb-16 relative overflow-hidden">
+    <section className="py-8 md:py-20 xl:pt-20 xl:pb-16 relative overflow-hidden bg-[#f2f2f2]">
       <div className="relative z-10 container flex flex-col md:flex-row items-center">
 
         {/* LEFT SIDE */}
-        <div className="w-full md:w-[44%] space-y-2 text-center ltr:md:text-left rtl:md:text-right">
-          <h2 className="text-[#000032] text-[32px] md:text-[30px] ltr:xl:text-[40px] rtl:xl:text-[30px] font-bold">
+        <div className="max-w-4xl mx-auto space-y-2 text-center mb-5">
+          <h2 className="text-[#000032] text-[22px] md:text-[30px] ltr:xl:text-[40px] rtl:xl:text-[30px] font-bold">
             {t("para1")}
           </h2>
 
-          <h1 className="ltr:text-[76px] ltr:md:text-[56px] ltr:xl:text-[76px]  ltr:2xl:text-[90px] rtl:text-[26px] rtl:md:text-[36px] rtl:xl:text-[46px]  rtl:2xl:text-[60px] font-bold leading-none bg-gradient-to-b from-[#E1CFBB] to-[#956D42] inline-block text-transparent bg-clip-text rtl:leading-normal z-50 relative">
+          <h1 className="ltr:text-[36px] ltr:md:text-[56px] ltr:xl:text-[80px]  ltr:2xl:text-[90px] rtl:text-[26px] rtl:md:text-[36px] rtl:xl:text-[46px]  rtl:2xl:text-[60px] font-bold leading-none bg-gradient-to-r from-[#956D42] to-[#956D42] inline-block text-transparent bg-clip-text rtl:leading-normal z-50 relative">
             {t("no")}
           </h1>
 
-          <h2 className="text-[#000032] text-[32px] md:text-[28px] xl:text-[40px] font-bold leading-tight px-5 md:px-0">
+          <h2 className="text-[#000032] text-[22px] md:text-[28px] xl:text-[40px] font-bold leading-tight px-5 md:px-0">
             {t("title")}
           </h2>
 
-          {/* Mobile Image */}
-          <div className="block md:hidden relative w-full h-[264px] mb-[-30px] md:mb-0">
-            <Image
-              src={imageSrc}
-              fill
-              alt="GTC Trophy"
-              className="object-contain"
-            />
-          </div>
+    
 
           {/* Paragraph */}
-          <p className="text-primary text-[14px] md:text-[16px] leading-relaxed py-4 md:px-0 mb-3 md:mb-0">
+          <p className="text-primary text-[16px] md:text-[20px] leading-relaxed py-4 md:px-0 mb-3 md:mb-0 max-w-2xl mx-auto">
             {t("para2")}
           </p>
 
-          {/* CTA Button */}
-          <div className="flex flex-row justify-center md:justify-start items-center gap-4 md:pb-20 md:px-0 mt-4">
-            <CustomButton
-              text={t("btnText")}
-              bgColor="bg-gradient-to-l from-[#E1CFBB] to-[#956D42] text-white w-full md:w-auto hover:bg-gradient-to-r hover:from-[#E1CFBB] hover:to-[#956D42]"
-              textColor="text-[#1F2937]"
-              strokeColor="#fff"
-              onClick={() => setIsOpen(true)}
-            />
-          </div>
+           <DepositFormNew />
         </div>
 
-        {/* RIGHT SIDE */}
-        <div className="hidden md:block md:w-[56%] 2xl:w-[65%] 2xl:-mr-28 3xl:-mr-36">
-          <DepositFormNew />
-        </div>
+  
       </div>
     </section>
   );
--- src/app/[locale]/5k-demo-trading-competition/deposit-match-bonus/page.jsx
+++ src/app/[locale]/5k-demo-trading-competition/deposit-match-bonus/page.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+'use client'
+import { useState } from "react";
 import HeroMargin from './components/HeroMargin'
+import { useTranslations } from 'next-intl'
+import Meta from '../../../components/common/MetaData'
+import MainHeader from '../../components/MainHeader'
+import FinalCtaSection from '../components/FinalCtaSection'
+import MainFooter from '../../components/MainFooter'
 
 const page = () => {
+   const [isOpen, setIsOpen] = useState(false);
+  const t = useTranslations("demo");
   return (
     <>
+    <Meta title={t('meta.title')} description={t('meta.description')}/>
+            <MainHeader />
     <HeroMargin />
+     <FinalCtaSection  setIsOpen={setIsOpen}  />
+    <MainFooter />
     </>
   )
 }
