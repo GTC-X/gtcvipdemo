db44c21dcabc9e17cac2388951d95f40862f1cf9
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-10-21 09:23:45 +0400

    ss asdjasd

--- src/app/[locale]/5k-demo-trading-competition/components/WhyJoinSection.jsx
+++ src/app/[locale]/5k-demo-trading-competition/components/WhyJoinSection.jsx
@@ -95,31 +95,34 @@ const WhyJoinSection = ({ setIsOpen, isAfterMidnight }) => {
 
   return (
     <section
-      className="relative bg-[#F7F3EE] pt-10 pb-16 overflow-hidden"
+      className="relative bg-[#F7F3EE] pt-10 pb-16 md:py-20 overflow-hidden"
       id="join"
     >
 
       <div className="relative z-10 container text-center">
         <div className="mb-12 flex flex-col gap-5">
-       
+          <h2 className="text-[25px] md:text-3xl xl:text-[40px] font-bold text-primary">
+            {"Leaderboard "}
+
+          </h2>
           {/* {!isAfterMidnight &&
             <p className="text-[#000021] text-base leading-5 px-5 md:px-0 max-w-[1192px] mx-auto mb-5">
               {tRegister("description")}
             </p>
-          } }
+          } */}
           {/* {!isAfterMidnight ?
             <SheetTable
               id="1gjvJ35RNXe-aDYANSu-9dg7vDsOztslaRseHTYJMFWU"
               gid="0"
             />
-            : }
-          
-          {/*   <iframe
+            : */}
+            <iframe
               src="https://prod-widgets.returning.ai/custom-leaderboards/67c95cd354f1f4d3292b5455/gtc-vip-demo-contest"
               frameborder="0"
               width="100%"
-              height="1000px"
-            ></iframe>} */}
+              height="100%"
+            ></iframe>
+          {/* } */}
         </div>
 
         <div className="flex flex-col items-center justify-center gap-6 md:gap-9 pb-8 md:pb-16">
@@ -150,7 +153,17 @@ const WhyJoinSection = ({ setIsOpen, isAfterMidnight }) => {
           ))}
         </div>
 
-      
+        <div className="flex justify-center">
+          <CustomButton
+            text={t("cta")}
+            bgColor="bg-white w-full md:w-auto hover:bg-gradient-to-l hover:from-[#E1CFBB] hover:to-[#956D42]"
+            textColor="text-[#B48755] hover:text-white"
+            strokeColor="#B48755"
+            disabled={true}
+            hoverStrokeColor="#FFFFFF"
+            onClick={() => setIsOpen(true)}
+          />
+        </div>
       </div>
 
       <style jsx>{`
